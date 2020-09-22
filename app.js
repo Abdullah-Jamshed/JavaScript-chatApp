fd982f4739e7a45b4e75f206d09c732046b73279
Abdullah Jamshed <abdullahjamshad01@gmail.com>
2020-09-22 23:08:59 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,73 +11,7 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-var contacts = document.querySelector(".contact ul");
-contacts.addEventListener("click", (e) => {
-    if (e.target.classList.contains("contact-list-item")) {
-        var start = document.querySelector(".start")
-        start.style.display = "none"
-        var panel = document.querySelector(".chatbox2")
-        panel.style.display = "inline-block"
-        var inp = document.querySelector(".message-input")
-        inp.value = ""
 
-        var code = e.target.getAttribute("code")
-        sessionStorage.setItem("chatPartnerCode", code)
-        chatPartner(code)
-
-        // var activeLi = document.querySelectorAll(".contact-list-item")
-        // Array.from(activeLi).forEach((li) => {
-        //     if (li === e.target) {
-        //         li.classList.add("active")
-        //     } else {
-        //         li.classList.remove("active")
-        //     }
-        // })
-
-        testFunc()
-    }
-})
-
-var popup = document.querySelector(".pop-up2")
-var addPopup = document.querySelector(".add-popup2")
-var addContact = document.querySelector(".add-contact")
-addContact.addEventListener("click", (e) => {
-    addContact.style.visibility = "hidden"
-    popup.style.display = "block"
-    addPopup.style.display = "block"
-})
-
-
-var cancel = document.querySelector(".fa-times")
-cancel.addEventListener("click", (e) => {
-    addContact.style.visibility = "visible"
-    popup.style.display = "none"
-    addPopup.style.display = "none"
-})
-
-var form = document.forms["form2"]
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    var name = document.querySelector("#name").value
-    var code = document.querySelector("#code").value
-    addContactToDatabase(name, code);
-    addContact.style.visibility = "visible"
-    popup.style.display = "none"
-    addPopup.style.display = "none"
-
-})
-
-function addContactToDatabase(name, code) {
-    var dataKey = firebase.database().ref("users").push().key
-    var key = sessionStorage.getItem("key")
-
-    function Contacts(name, code) {
-        this.name = name
-        this.code = code
-    }
-    var contact = new Contacts(name, code)
-    firebase.database().ref(`users/${key}/contacts/${dataKey}`).set(contact)
-}
 
 var login = document.forms["form1"]
 login.addEventListener("submit", (e) => {
@@ -86,6 +20,8 @@ login.addEventListener("submit", (e) => {
     loginBtn.disabled = true
     matchCode()
 })
+
+
 
 function matchCode() {
     var code = document.querySelector("#login-code")
@@ -113,6 +49,7 @@ function matchCode() {
 }
 
 
+
 function fetchDatabase() {
     var ul = document.querySelector(".contact-ul")
     var key = sessionStorage.getItem("key")
@@ -135,6 +72,49 @@ function fetchDatabase() {
 }
 
 
+var popup = document.querySelector(".pop-up2")
+var addPopup = document.querySelector(".add-popup2")
+var addContact = document.querySelector(".add-contact")
+addContact.addEventListener("click", () => {
+    addContact.style.visibility = "hidden"
+    popup.style.display = "block"
+    addPopup.style.display = "block"
+})
+
+var cancel = document.querySelector(".fa-times")
+cancel.addEventListener("click", (e) => {
+    addContact.style.visibility = "visible"
+    popup.style.display = "none"
+    addPopup.style.display = "none"
+})
+
+
+function addContactToDatabase(name, code) {
+    var dataKey = firebase.database().ref("users").push().key
+    var key = sessionStorage.getItem("key")
+
+    function Contacts(name, code) {
+        this.name = name
+        this.code = code
+    }
+    var contact = new Contacts(name, code)
+    firebase.database().ref(`users/${key}/contacts/${dataKey}`).set(contact)
+}
+
+
+var form = document.forms["form2"]
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    var name = document.querySelector("#name").value
+    var code = document.querySelector("#code").value
+    addContactToDatabase(name, code);
+    addContact.style.visibility = "visible"
+    popup.style.display = "none"
+    addPopup.style.display = "none"
+
+})
+
+
 function chatPartner(code) {
     var key = sessionStorage.getItem("key")
     firebase.database().ref(`users/${key}/contacts`).orderByChild("code").equalTo(code).once("value", (data) => {
@@ -145,9 +125,32 @@ function chatPartner(code) {
     })
 }
 
+var contacts = document.querySelector(".contact ul");
+contacts.addEventListener("click", (e) => {
+    if (e.target.classList.contains("contact-list-item")) {
+        var messagePanel = document.querySelector(".message-panel")
+        messagePanel.innerHTML = ""
+        var start = document.querySelector(".start")
+        start.style.display = "none"
+        var panel = document.querySelector(".chatbox2")
+        panel.style.display = "inline-block"
+        var inp = document.querySelector(".message-input")
+        inp.value = ""
+
+        var code = e.target.getAttribute("code")
+        sessionStorage.setItem("chatPartnerCode", code)
+        chatPartner(code)
+        firstRender()
+        var adminKey = sessionStorage.getItem("key")
+        var obj = { "code": code }
+        firebase.database().ref(`users/${adminKey}/currentConnect/`).update(obj)
+    }
+})
+
+
 
 var sentMessage = document.querySelector(".fa-paper-plane")
-sentMessage.addEventListener("click", (e) => {
+sentMessage.addEventListener("click", () => {
     var input = document.querySelector("#message-text")
     var inputValue = input.value
     if (inputValue !== "") {
@@ -164,56 +167,111 @@ sentMessage.addEventListener("click", (e) => {
         var time = new Date().getTime()
         firebase.database().ref(`messages/${time}/`).set(message)
 
+        lastMessage()
     }
 
     input.value = ""
-
 })
 
 
+function lastMessage() {
+    var messagePanel = document.querySelector(".message-panel")
+    var admin = sessionStorage.getItem("code")
+    var chatPartner = sessionStorage.getItem("chatPartnerCode")
+    firebase.database().ref("messages").orderByChild("sender").equalTo(admin).limitToLast(1).once("value", (data) => {
+        let Data = data.val()
+        for (let prop in data.val()) {
+            let content = Data[prop]["content"]
+            let sender = Data[prop]["sender"]
+            let to = Data[prop]["to"]
+            if (sender === admin && to === chatPartner) {
+                var div = document.createElement("div")
+                var li = document.createElement("li")
+                li.textContent = content
+                div.appendChild(li)
+                div.classList.add("admin-li")
+                messagePanel.appendChild(div)
+            }
+        }
+    })
+}
 
 
 
-function testFunc() {
-
+function firstRender() {
     var messagePanel = document.querySelector(".message-panel")
     messagePanel.innerHTML = ""
-    firebase.database().ref("messages").on("child_added", (data) => {
+    var admin = sessionStorage.getItem("code")
+    var chatPartner = sessionStorage.getItem("chatPartnerCode")
+    firebase.database().ref("messages").once("value", (data) => {
         var Data = data.val()
-        var content = Data["content"]
-        var sender = Data["sender"]
-        var to = Data["to"]
-        var admin = sessionStorage.getItem("code")
-        var chatPartner = sessionStorage.getItem("chatPartnerCode")
+        for (let props in Data) {
+            var content = Data[props]["content"]
+            var sender = Data[props]["sender"]
+            var to = Data[props]["to"]
 
-        if (sender === admin && to === chatPartner) {
-            console.log("condition 1");
-            var div = document.createElement("div")
-            var li = document.createElement("li")
-            li.textContent = content
-            div.appendChild(li)
-            if (sender === admin) {
-                div.classList.add("admin-li")
-            } else if (sender === chatPartner) {
-                div.classList.add("partner-li")
+            if (sender === admin && to === chatPartner) {
+                var div = document.createElement("div")
+                var li = document.createElement("li")
+                li.textContent = content
+                div.appendChild(li)
+                if (sender === admin) {
+                    div.classList.add("admin-li")
+                } else if (sender === chatPartner) {
+                    div.classList.add("partner-li")
+                }
+                messagePanel.appendChild(div)
+            } else if (sender === chatPartner && to === admin) {
+                var div = document.createElement("div")
+                var li = document.createElement("li")
+                li.textContent = content
+                div.appendChild(li)
+                if (sender === admin) {
+                    div.classList.add("admin-li")
+                } else if (sender === chatPartner) {
+                    div.classList.add("partner-li")
+                }
+                messagePanel.appendChild(div)
             }
-            messagePanel.appendChild(div)
-        } else if (sender === chatPartner && to === admin) {
-            console.log("condition 2");
-            var div = document.createElement("div")
-            var li = document.createElement("li")
-            li.textContent = content
-            div.appendChild(li)
-            if (sender === admin) {
-                div.classList.add("admin-li")
-            } else if (sender === chatPartner) {
-                div.classList.add("partner-li")
-            }
-            messagePanel.appendChild(div)
         }
 
+
     })
+}
 
-firebase}
 
+var admin = sessionStorage.getItem("code")
+var chatPartnerCode = sessionStorage.getItem("chatPartnerCode")
+var messagePanel = document.querySelector(".message-panel")
+firebase.database().ref("messages").limitToLast(1).on("child_added", (data) => {
+    var Data = data.val()
+    var content = Data["content"]
+    var sender = Data["sender"]
+    var to = Data["to"]
 
+    if (sender === admin && to === chatPartnerCode) {
+        console.log("yes");
+        var div = document.createElement("div")
+        var li = document.createElement("li")
+        li.textContent = content
+        div.appendChild(li)
+        if (sender === admin) {
+            div.classList.add("admin-li")
+        } else if (sender === chatPartner) {
+            div.classList.add("partner-li")
+        }
+        messagePanel.appendChild(div)
+    } else if (sender === chatPartnerCode && to === admin) {
+        console.log("no");
+        var div = document.createElement("div")
+        var li = document.createElement("li")
+        li.textContent = content
+        div.appendChild(li)
+        if (sender === admin) {
+            div.classList.add("admin-li")
+        } else if (sender === chatPartnerCode) {
+            div.classList.add("partner-li")
+        }
+        messagePanel.appendChild(div)
+    }
+})
